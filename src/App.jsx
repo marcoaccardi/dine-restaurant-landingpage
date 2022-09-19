82fcc27d9c83eb6088c5eedc566bd7d42b17d78e
sonotype <contact.marco.accardi@gmail.com>
2022-09-19 14:48:06 +0200

    setup App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+import Footer from "./components/Footer";
 import "./styles/main.scss";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path='/' component={<Home />} />
+          <Route exact path='/booking' component={<Booking />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
